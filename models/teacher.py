c418de3f24d19e31d4ee37cfc4bf5eee3ea56828
chaturdarshan <chaturdarshan11@gmail.com>
2024-05-23 02:12:42 +0530

    Create teacher.py

---
+++ models/teacher.py
@@ -0,0 +1,10 @@
+class Teacher:
+    @staticmethod
+    def get_all_teachers():
+        # Fetch all teachers from database
+        pass
+
+    @staticmethod
+    def get_teacher_by_id(teacher_id):
+        # Fetch teacher by ID from database
+        pass
