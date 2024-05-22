93c55f641e2818d5679f7224d7971db973669e1b
chaturdarshan <chaturdarshan11@gmail.com>
2024-05-23 02:10:57 +0530

    Create python app.py

---
+++ python app.py
@@ -0,0 +1,10 @@
+- Open the app in your web browser and start connecting with teachers.
+
+## Contributors
+
+- Prof. Supriya Jadhav
+- Team of 4 developers
+
+## License
+
+This project is licensed under the MIT License.
